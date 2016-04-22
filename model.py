4c635f543a785ccf9474867d7561456937688497
Peter Tonner <peter.tonner@gmail.com>
2016-04-22 13:59:27 -0400

    generalize to multpiple effects, and prior sampling

--- model.py
+++ model.py
@@ -4,6 +4,8 @@ import GPy, scipy
 
 class GP_FANOVA(object):
 
+	EFFECT_SUFFIXES = ['alpha','beta','gamma']
+
 	def __init__(self,x,y,effect):
 		"""
 
@@ -18,31 +20,98 @@ class GP_FANOVA(object):
 		self.sample_x = np.unique(self.x)[:,None] # this only works for one dimension
 
 		self.n = x.shape[0]
-		assert self.n == self.y.shape[0], 'x and y must be same length'
+		# assert self.n == self.y.shape[0], 'x and y must be same length'
 
 		self.sample_n = self.sample_x.shape[0]
 
 		self.p = x.shape[1]
 
-		self.k = np.unique(self.effect).shape[0]
-		self.nk = [sum(self.effect == i) for i in range(self.k)]
+		self.k = self.effect.shape[1] # number of effects
+		self.mk = [np.unique(self.effect[:,i]).shape[0] for i in range(self.k)] # number of levels for each effect
+		# self.nk = {[(i,[sum(self.effect[:,i] == j) for j in range(self.mk[i])]) for i in range(self.k)]} # number of observations for each effect level
 		self.nt = self.y.shape[1]
 
 		alpha_cols = []
 		for i in range(self.k):
 			alpha_cols = alpha_cols + ['alpha_%d(%lf)'%(i,z) for z in self.sample_x]
 
-		# mu and alpha samples, kernel hyperparams (mu, alpha, noise)
-		self.parameter_history = pd.DataFrame(np.zeros(self.sample_n*(1+self.k)+6)[None,:],
-									columns=['mu(%lf)'%z for z in self.sample_x]+
-											alpha_cols+
-											['mu_sigma','mu_lengthscale',
-											'alpha_sigma','alpha_lengthscale',
-											'y_sigma','y_lengthscale'])
-		self.parameter_history.iloc[0,-6:] = 1
-		self.parameter_history.loc[0,['y_sigma']] = .1
+		ind = self.mu_index() + ['mu_sigma','mu_lengthscale']
+		for i in range(self.k):
+			for j in range(self.mk[i]):
+				ind += self.effect_index(i,j)
+			ind += ["%s_sigma"%GP_FANOVA.EFFECT_SUFFIXES[i],"%s_lengthscale"%GP_FANOVA.EFFECT_SUFFIXES[i]]
 
-		self.parameter_cache = self.parameter_history.iloc[0,:]
+			# add interaction samples
+			for k in range(i):
+				for l in range(self.mk[i]):
+					for m in range(self.mk[k]):
+						ind += self.effect_interaction_index(i,l,k,m)
+				ind += ["%s:%s_sigma"%(GP_FANOVA.EFFECT_SUFFIXES[i],GP_FANOVA.EFFECT_SUFFIXES[k]),
+						"%s:%s_lengthscale"%(GP_FANOVA.EFFECT_SUFFIXES[i],GP_FANOVA.EFFECT_SUFFIXES[k])]
+
+		ind += ['y_sigma','y_lengthscale']
+
+		# print self.sample_n*(1+sum(self.mk))+4+self.k*2, len(ind)
+		self.parameter_cache = pd.Series(np.zeros(len(ind)),
+											index=ind)
+
+		self.parameter_cache[['mu_sigma','mu_lengthscale','y_sigma','y_lengthscale']] = 1
+		for i in range(self.k):
+			self.parameter_cache[['%s_sigma'%GP_FANOVA.EFFECT_SUFFIXES[i],'%s_lengthscale'%GP_FANOVA.EFFECT_SUFFIXES[i]]] = 1
+		self.parameter_cache['y_sigma'] = .1
+
+		self.parameter_history = pd.DataFrame(columns=ind)
+
+		# mu and alpha samples, kernel hyperparams (mu, alpha, noise)
+		# self.parameter_history = pd.DataFrame(np.zeros(self.sample_n*(1+self.k)+6)[None,:],
+		# 							columns=['mu(%lf)'%z for z in self.sample_x]+
+		# 									alpha_cols+
+		# 									['mu_sigma','mu_lengthscale',
+		# 									'alpha_sigma','alpha_lengthscale',
+		# 									'y_sigma','y_lengthscale'])
+		# self.parameter_history.iloc[0,-6:] = 1
+		# self.parameter_history.loc[0,['y_sigma']] = .1
+		#
+		# self.parameter_cache = self.parameter_history.iloc[0,:]
+
+	def offset(self):
+		return 1e-9
+
+	def sample_prior(self):
+		# mean
+		mu,cov = np.zeros(self.n), self.mu_k().K(self.x) + np.eye(self.n)*self.offset()
+		mean = scipy.stats.multivariate_normal.rvs(mu,cov)
+
+		# effects
+		effects = []
+		for i in range(self.k):
+			effects.append(np.zeros((self.n,self.mk[i])))
+			for j in range(self.mk[i]):
+				if j == self.mk[i] - 1:
+					sample = -np.sum(effects[i],1)
+					effects[i][:,j] = sample
+				mu = np.zeros(self.n)
+				mu -= np.sum(effects[i][:,:j],1) / (self.mk[i] - j)
+				cov = 1.*(self.mk[i]-j-1)/(self.mk[i]-j) * self.effect_k(i).K(self.x) + np.eye(self.n)*self.offset()
+				sample = scipy.stats.multivariate_normal.rvs(mu,cov)
+				effects[i][:,j] = sample
+
+		# noise
+		obs = np.zeros((self.n,self.nt))
+		for i in range(self.nt):
+			mu = mean.copy()
+			for j in range(self.k):
+				mu+=effects[j][:,self.effect[i,j]]
+			cov = self.y_k().K(self.x)
+			obs[:,i] = scipy.stats.multivariate_normal.rvs(mu,cov)
+
+		return mean,effects,obs
+
+	def effect_index(self,k,l,):
+		return ['%s_%d(%lf)'%(GP_FANOVA.EFFECT_SUFFIXES[k],l,z) for z in self.sample_x]
+
+	def effect_interaction_index(self,k,l,m,n):
+		return ['%s_%d:%s_%d(%lf)'%(GP_FANOVA.EFFECT_SUFFIXES[k],l,GP_FANOVA.EFFECT_SUFFIXES[m],n,z) for z in self.sample_x]
 
 	def mu_index(self):
 		return ['mu(%lf)'%z for z in self.sample_x]
@@ -60,6 +129,14 @@ class GP_FANOVA(object):
 
 	def mu_k(self):
 		sigma,ls = self.parameter_cache[['mu_sigma','mu_lengthscale']]
+		return GPy.kern.RBF(self.p,variance=sigma,lengthscale=ls)
+
+	def effect_k(self,i):
+		sigma,ls = self.parameter_cache[["%s_sigma"%GP_FANOVA.EFFECT_SUFFIXES[i],"%s_lengthscale"%GP_FANOVA.EFFECT_SUFFIXES[i]]]
+		return GPy.kern.RBF(self.p,variance=sigma,lengthscale=ls)
+
+	def effect_interaction_k(self,i):
+		sigma,ls = self.parameter_cache[["%s:%s_sigma"%(GP_FANOVA.EFFECT_SUFFIXES[i],GP_FANOVA.EFFECT_SUFFIXES[j]),"%s_lengthscale"%(GP_FANOVA.EFFECT_SUFFIXES[i],GP_FANOVA.EFFECT_SUFFIXES[j])]]
 		return GPy.kern.RBF(self.p,variance=sigma,lengthscale=ls)
 
 	def alpha_k(self):
@@ -159,6 +236,7 @@ class GP_FANOVA(object):
 
 		# plt.gca().set_color_cycle(None)
 		colors = [u'b', u'g', u'r', u'c', u'm', u'y',]
+		cmaps = ["Blues",'Greens','Reds']
 
 		for i in range(self.k):
 			if offset:
@@ -176,10 +254,16 @@ class GP_FANOVA(object):
 			plt.plot(self.sample_x,mean,'k')
 			plt.fill_between(self.sample_x[:,0],mean-2*std,mean+2*std,alpha=.2,color='k')
 
-	def plot_data(self):
+	def plot_data(self,alpha=1,offset=1):
 		import matplotlib.pyplot as plt
 
 		colors = [u'b', u'g', u'r', u'c', u'm', u'y']
+		cmaps = [plt.get_cmap(c) for c in ["Blues",'Greens','Reds']]
 
 		for i in range(self.y.shape[1]):
-			plt.plot(self.x,self.y[:,i],color=colors[self.effect[i]],alpha=.6)
+			if self.k == 2:
+				c = cmaps[self.effect[i,0]](1.*(self.effect[i,1] + offset)/(self.mk[1]+2*offset))
+			else:
+				c = colors(self.effect[i,0])
+
+			plt.plot(self.x,self.y[:,i],color=c,alpha=alpha)
