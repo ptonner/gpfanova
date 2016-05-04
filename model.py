536a9d8521f57bc72e9019f0777ff66893c4d71e
Peter Tonner <peter.tonner@gmail.com>
2016-05-04 10:35:42 -0400

    fix interaction sampling, add some plotting fxns

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import GPy, scipy, time
+import matplotlib.pyplot as plt
 
 class GP_FANOVA(object):
 
@@ -28,7 +29,13 @@ class GP_FANOVA(object):
 
 		self.k = self.effect.shape[1] # number of effects
 		self.mk = [np.unique(self.effect[:,i]).shape[0] for i in range(self.k)] # number of levels for each effect
+
 		self.nk = dict([(i,[sum(self.effect[:,i] == j) for j in range(self.mk[i])]) for i in range(self.k)]) # number of observations for each effect level
+		if self.k >= 2:
+			for j in range(self.mk[0]):
+				for l in range(self.mk[1]):
+					self.nk[(j,l)] = sum(np.all(self.effect[:,[0,1]]==(j,l),1))
+
 		self.nt = self.y.shape[1]
 
 		# alpha_cols = []
@@ -51,6 +58,7 @@ class GP_FANOVA(object):
 						"%s:%s_lengthscale"%(GP_FANOVA.EFFECT_SUFFIXES[k],GP_FANOVA.EFFECT_SUFFIXES[i])]
 
 		ind += ['y_sigma','y_lengthscale']
+		ind += ['log_likelihood']
 
 		# print self.sample_n*(1+sum(self.mk))+4+self.k*2, len(ind)
 		self.parameter_cache = pd.Series(np.zeros(len(ind)),
@@ -332,15 +340,20 @@ class GP_FANOVA(object):
 		else:
 			mu = - sub_effect_1 - sub_effect_2
 
-		mu -= mu/(self.mk[i]-j)/(self.mk[k]-l)
+		mu = mu/(self.mk[i]-j)/(self.mk[k]-l)
 		cov = 1.*(self.mk[i]-j-1)/(self.mk[i]-j)*(self.mk[k]-l-1)/(self.mk[k]-l) * self.effect_interaction_k(i,k).K(self.sample_x) + np.eye(self.sample_n)*self.offset()
+
+		cov_inv = np.linalg.inv(cov)
 
 		# print mu,cov
 
 		y_k_inv = np.linalg.inv(self.y_k().K(self.sample_x))
 
-		A = cov + self.nk[i][j] * self.nk[k][l] * y_k_inv
-		b = self.nk[i][j]*self.nk[k][l]*np.dot(y_k_inv,self.y_sub_mu_for_interaction(i,j,k,l)) + np.dot(cov,mu)
+		# A = cov + self.nk[i][j] * self.nk[k][l] * y_k_inv
+		# b = self.nk[i][j]*self.nk[k][l]*np.dot(y_k_inv,self.y_sub_mu_for_interaction(i,j,k,l)) + np.dot(cov,mu)
+
+		A = cov_inv + self.nk[(j,l)] * y_k_inv
+		b = self.nk[(j,l)]*np.dot(y_k_inv,self.y_sub_mu_for_interaction(i,j,k,l)) + np.dot(cov_inv,mu)
 
 		A_inv = np.linalg.inv(A)
 		return np.dot(A_inv,b), A_inv
@@ -458,6 +471,9 @@ class GP_FANOVA(object):
 
 		# interactions
 
+		# store the likelihood
+		self.parameter_cache['log_likelihood'] = self.likelihood()
+
 	def store(self):
 		self.parameter_history = self.parameter_history.append(self.parameter_cache,ignore_index=True)
 		self.parameter_history.index = range(self.parameter_history.shape[0])
@@ -510,7 +526,6 @@ class GP_FANOVA(object):
 			plt.fill_between(self.sample_x[:,0],mean-2*std,mean+2*std,alpha=.2,color='k')
 
 	def plot_data(self,alpha=1,offset=1):
-		import matplotlib.pyplot as plt
 
 		colors = [u'b', u'g', u'r', u'c', u'm', u'y']
 		cmaps = [plt.get_cmap(c) for c in ["Blues",'Greens','Reds']]
@@ -518,12 +533,127 @@ class GP_FANOVA(object):
 		# first effect is color,
 		# second effect is subplot
 
-		for i in range(self.y.shape[1]):
-			if self.k >= 2:
-				plt.subplot(1,self.mk[1],self.effect[i,1]+1)
-				# c = cmaps[self.effect[i,0]](1.*(self.effect[i,1] + offset)/(self.mk[1]+2*offset))
-			# else:
-			c = colors[self.effect[i,0]]
+		if self.mk[0] <= len(colors):
+			for i in range(self.y.shape[1]):
+				if self.k >= 2:
+					plt.subplot(1,self.mk[1],self.effect[i,1]+1)
+					# c = cmaps[self.effect[i,0]](1.*(self.effect[i,1] + offset)/(self.mk[1]+2*offset))
+				# else:
+				c = colors[self.effect[i,0]]
 
-			plt.plot(self.x,self.y[:,i],color=c,alpha=alpha)
-			plt.ylim(self.y.min(),self.y.max())
+				plt.plot(self.x,self.y[:,i],color=c,alpha=alpha)
+				plt.ylim(self.y.min(),self.y.max())
+		else:
+			for i in range(self.mk[0]):
+				y = self.y[:,self.effect[:,0]==i]
+				plt.plot(self.x,y.mean(1),c='k',alpha=alpha)
+				plt.fill_between(self.x[:,0],y.mean(1)-y.std(1),y.mean(1)+y.std(1),color='k',alpha=.2)
+
+
+	def plot_two_effects(self,i,k,_data=True,function=False,samples=True,offset=True,subplot_dims=None,data_plot_kwargs={}):
+		colors = [u'b', u'g', u'r', u'c', u'm', u'y']
+
+		if subplot_dims is None:
+			subplot_dims = range(2)
+
+		if i in subplot_dims:
+			nrows = self.mk[i]
+		else:
+			nrows = 1
+		if k in subplot_dims:
+			ncols = self.mk[k]
+		else:
+			ncols = 1
+
+
+		for j in range(self.mk[i]):
+			for l in range(self.mk[k]):
+
+				if i in subplot_dims and k in subplot_dims:
+					pos = self.mk[k]*j + l+1
+					c = 'k'
+				elif i in subplot_dims:
+					pos = j+1
+					c = colors[l]
+				elif k in subplot_dims:
+					pos = l+1
+					c = colors[j]
+				else:
+					pos = 1
+					c = colors[j*self.mk[i]+l]
+				plt.subplot(nrows,ncols,pos)
+
+				if i in subplot_dims and k in subplot_dims:
+					plt.title("%d, %d" % (j,l))
+
+				if _data:
+					data = self.y[:,np.all(self.effect[:,[i,k]]==(j,l),1)]
+					mean = data.mean(1)
+					std = data.std(1)
+
+					if samples:
+						plt.plot(self.x[:,0],data,c,**data_plot_kwargs)
+					else:
+						plt.plot(self.x[:,0],mean,c)
+						plt.fill_between(self.x[:,0],mean-2*std,mean+2*std,color=c,alpha=.2)
+
+				if function:
+					data = self.parameter_history[self.effect_index(i,j)].values + self.parameter_history[self.effect_index(k,l)].values + self.parameter_history[self.effect_interaction_index(k,l,i,j)].values
+					if offset:
+						data += self.parameter_history[self.mu_index()].values
+					mean = data.mean(0)
+					std = data.std(0)
+
+					plt.plot(self.sample_x,mean,c)
+					plt.fill_between(self.sample_x[:,0],mean-2*std,mean+2*std,color=c,alpha=.2)
+
+				plt.ylim(self.y.min(),self.y.max())
+
+				if function and not offset:
+					plt.ylim(mean.min()-std.max()*2,mean.max()+std.max()*2)
+
+	def plot_effect(self,subplots=True,title_kwargs={}):
+		for i in range(self.k):
+			for j in range(self.mk[i]):
+
+				if subplots:
+					plt.subplot(max(self.mk),self.k,i+1+self.k*j)
+					plt.title("$\\%s_%d$"%(GP_FANOVA.EFFECT_SUFFIXES[i],j),**title_kwargs)
+
+				data = self.parameter_history[self.effect_index(i,j)].values
+				# if offset:
+				# 	data += self.parameter_history[self.mu_index()].values
+				mean = data.mean(0)
+				std = data.std(0)
+
+				plt.plot(self.sample_x,mean)
+				plt.fill_between(self.sample_x[:,0],mean-2*std,mean+2*std,alpha=.2)
+
+	def plot_hyper_params(self):
+
+		def plot_param(param):
+			plt.plot(self.parameter_history[param])
+			plt.title(param,fontsize=20)
+
+		plt.subplot(5,2,1)
+		plot_param('mu_lengthscale')
+		plt.subplot(5,2,2)
+		plot_param('mu_sigma')
+
+		plt.subplot(5,2,3)
+		plot_param('alpha_lengthscale')
+		plt.subplot(5,2,4)
+		plot_param('alpha_sigma')
+
+		plt.subplot(5,2,5)
+		plot_param('beta_lengthscale')
+		plt.subplot(5,2,6)
+		plot_param('beta_sigma')
+
+		plt.subplot(5,2,7)
+		plot_param('alpha:beta_lengthscale')
+		plt.subplot(5,2,8)
+		plot_param('alpha:beta_sigma')
+
+		plt.subplot(5,2,9)
+		plot_param('y_sigma')
