4f2222a56db45e90966a73172e6a46e156f4729a
Peter Tonner <peter.tonner@gmail.com>
2016-06-08 16:16:34 -0400

    make into better script

--- profile.py
+++ profile.py
@@ -6,12 +6,28 @@ import pstats, cProfile
 
 import data,gp_fanova
 
-x,y,effect = data.hsalinarum_strain_data(['trmB','rosR'])
-m = gp_fanova.base.GP_FANOVA(x,y,effect)
-def profile():
-	m.sample(500,1)
+def profile(m,n=10):
+	m.sample(n,1)
 
-cProfile.runctx("profile()", globals(), locals(), "Profile.prof")
+if __name__ == "__main__":
+	import sys, getopt
 
-s = pstats.Stats("Profile.prof")
-s.strip_dirs().sort_stats("time").print_stats()
+	opt,args = getopt.getopt(sys.argv[1:],'n:')
+
+	n = 10
+	d = "beer"
+
+	for o,a in opt:
+		if o == '-n':
+			n = int(a)
+
+	if d == "beer":
+		x,y,effect,_ = data.hsalinarum_beer_data()
+	elif d == "strain":
+		x,y,effect = data.hsalinarum_strain_data(['trmB','rosR'])
+	m = gp_fanova.fanova.FANOVA(x,y,effect,hyperparam_kwargs={'y_sigma':(.1,10),'sigma':(.3,10),'lengthscale':(.3,10)})
+
+	cProfile.runctx("profile(m)", globals(), locals(), "Profile.prof")
+
+	s = pstats.Stats("Profile.prof")
+	s.strip_dirs().sort_stats("time").print_stats()
