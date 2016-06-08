096c8915387ac1592312e7c8947a360c44795903
Peter Tonner <peter.tonner@gmail.com>
2016-06-08 16:15:57 -0400

    plotting updates

--- gp_fanova/plot/__init__.py
+++ gp_fanova/plot/__init__.py
@@ -1,3 +1,3 @@
 from single_effect import plot_single_effect
 from hyperparams import plot_hyper_params
-from function import plot_function
+from function import plot_function, plot_functions
--- gp_fanova/plot/function.py
+++ gp_fanova/plot/function.py
@@ -1,8 +1,19 @@
 from meta import *
+import numpy as np
 
 def plot_function(m,f,c='b',alpha=.2,burnin=0):
 
 	samples = m.parameter_history[m.function_index(f)].values[burnin:,:]
+
+	mean = samples.mean(0)
+	std = samples.std(0)
+
+	plt.plot(m.x,mean,color=c)
+	plt.fill_between(m.x[:,0],mean-2*std,mean+2*std,alpha=alpha,color=c)
+
+def plot_functions(m,f,c='b',alpha=.2,burnin=0):
+
+	samples = np.sum([f[j] * m.parameter_history[m.function_index(j)].values[burnin:,:] for j in range(len(f))],0)
 
 	mean = samples.mean(0)
 	std = samples.std(0)
