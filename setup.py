a7df6543dcab532880e49b513eea9816a39711e2
Peter Tonner <peter.tonner@gmail.com>
2016-06-08 16:16:17 -0400

    temporary cython change

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from Cython.Build import cythonize
 
 setup(
   name = 'GP_FANOVA',
-  ext_modules = cythonize(["gp_fanova/*.pyx","gp_fanova/sample/*.pyx","gp_fanova/kernel/*.pyx"]),
+  # ext_modules = cythonize(["gp_fanova/*.pyx","gp_fanova/sample/*.pyx","gp_fanova/kernel/*.pyx"]),
+  ext_modules = cythonize(["gp_fanova/sample/*.pyx","gp_fanova/kernel/*.pyx"]),
 )
