22af037a849d4b45d1abbe810fa573d56790df84
Peter Tonner <peter.tonner@gmail.com>
2016-06-10 13:32:48 -0400

    script update

--- hsal_salt.py
+++ hsal_salt.py
@@ -3,16 +3,22 @@ if __name__ == "__main__":
 	import sys, getopt
 	import gp_fanova, data
 
-	opts,args = getopt.getopt(sys.argv[1:],'n:')
+	opts,args = getopt.getopt(sys.argv[1:],'n:i')
 
 	n = 10
+	interaction=False
 	for o,a in opts:
 		if o == '-n':
 			n = int(a)
+		if o =='-i':
+			interaction=True
 
 	x,y,effect,_ = data.hsalinarum_beer_data()
-	m = gp_fanova.fanova.FANOVA(x,y,effect)
+	m = gp_fanova.fanova.FANOVA(x,y,effect,interaction=interaction)
 
 	m.sample(n,1,random=True)
 
-	m.parameter_history.to_csv("results/hsal_beer_%d.csv"%n,index=False)
+	s = ""
+	if interaction:
+		s = "interaction_"
+	m.parameter_history.to_csv("results/hsal_beer_%s%d.csv"%(s,n),index=False)
