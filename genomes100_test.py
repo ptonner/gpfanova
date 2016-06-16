653715f69594fc7b6964940aef3dbfe307c1491a
Peter Tonner <peter.tonner@gmail.com>
2016-06-16 14:20:12 -0400

    Change simulated data structure

--- genomes100_test.py
+++ genomes100_test.py
@@ -7,32 +7,37 @@ if __name__ == "__main__":
 
 	import gp_fanova, data
 	import matplotlib.pyplot as plt
+	import numpy as np
 
 	opts,args = getopt.getopt(sys.argv[1:],'n:v:l:')
 
-	x,y,effect = data.one_effect_data(100)
+	x,y,effect,_ = data.multiple_effects([3]*5,100,20,False,seed=True)
 	m = gp_fanova.fanova.FANOVA(x,y,effect,helmert_convert=True)
 	m.parameter_cache['y_sigma'] = -1
+	m.parameter_cache['prior0_lengthscale'] = -1
+	for i in range(5):
+		m.parameter_cache['prior%d_lengthscale'%(i+1)] = np.random.uniform(-3,.5)
 
 	n = 1
 	for o,a in opts:
 		if o == '-n':
 			n = int(a)
-		elif o == '-v':
-			v0,v1 = a.split(',')
-			m.parameter_cache['prior0_sigma'] = float(v0)
-			m.parameter_cache['prior1_sigma'] = float(v1)
-		elif o == '-l':
-			v0,v1 = a.split(',')
-			m.parameter_cache['prior0_lengthscale'] = float(v0)
-			m.parameter_cache['prior1_lengthscale'] = float(v1)
+		# elif o == '-v':
+		# 	v0,v1 = a.split(',')
+		# 	m.parameter_cache['prior0_sigma'] = float(v0)
+		# 	m.parameter_cache['prior1_sigma'] = float(v1)
+		# elif o == '-l':
+		# 	v0,v1 = a.split(',')
+		# 	m.parameter_cache['prior0_lengthscale'] = float(v0)
+		# 	m.parameter_cache['prior1_lengthscale'] = float(v1)
 
-	m.y = m.sample_prior()
+	m.y,_ = m.sample_prior()
 
-
-	plt.figure(figsize=(20,20))
-	gp_fanova.plot.plot_single_effect(m,0,data=True,_mean=False,offset=False,alpha=1);
-	plt.savefig("results/genomes100_data.png",bbox_inches="tight",dpi=300)
+	for i in range(5):
+		# plt.figure(figsize=(20,20))
+		gp_fanova.plot.plotSingleEffect(m,i,data=True,_mean=False,offset=False,alpha=1);
+		plt.savefig("results/genomes100_data_%d.png"%i,bbox_inches="tight",dpi=300)
+		plt.close()
 
 	m.sample(n,10,random=True)
 
