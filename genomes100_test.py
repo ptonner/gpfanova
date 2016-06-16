44d5c9a0e0608cab1dc98466c63d4dbe8a1f5a48
ptonner <peter.tonner@duke.edu>
2016-06-16 13:27:02 -0400

    updates

--- genomes100_test.py
+++ genomes100_test.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
 
 	opts,args = getopt.getopt(sys.argv[1:],'n:v:l:')
 
-	x,y,effect = data.one_effect_data(100)
+	x,y,effect = data.one_effect_data(100,10)
 	m = gp_fanova.fanova.FANOVA(x,y,effect)
 	m.parameter_cache['y_sigma'] = -1
 
@@ -27,11 +27,11 @@ if __name__ == "__main__":
 			m.parameter_cache['prior0_lengthscale'] = float(v0)
 			m.parameter_cache['prior1_lengthscale'] = float(v1)
 
-	m.y = m.sample_prior()
+	m.y,_ = m.sample_prior()
 
 
 	plt.figure(figsize=(20,20))
-	gp_fanova.plot.plot_single_effect(m,0,data=True,_mean=False,offset=False,alpha=1);
+	gp_fanova.plot.plotSingleEffect(m,0,data=True,_mean=False,offset=False,alpha=1);
 	plt.savefig("results/genomes100_data.png",bbox_inches="tight",dpi=300)
 
 	m.sample(n,10,random=True)
