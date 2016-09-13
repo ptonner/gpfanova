c0bafe99c6bad9872148921519536ebe2dfba362
Peter <peter.tonner@duke.edu>
2016-09-13 13:51:55 -0400

    add some imports

--- gpfanova/__init__.py
+++ gpfanova/__init__.py
@@ -7,6 +7,10 @@ import sample
 import logging
 import interval
 
+from base import Base
+from prior import Prior
+from fanova import FANOVA
+
 logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
