0f4396f2db7cc9cc5685a7626e5788502696c8e2
Peter <peter.tonner@duke.edu>
2016-09-16 17:13:42 -0400

    add function interval

--- gpfanova/interval.py
+++ gpfanova/interval.py
@@ -5,6 +5,8 @@ class Interval(object):
 	def __init__(self,samples,alpha,ndim=1):
 		self.samples = samples
 		self.n = self.samples.shape[0]
+
+		# DOES ALPHA MEAN INCLUSIVE OR EXCLUSIVE PROB????
 		self.alpha = alpha
 
 		if self.alpha < 0 or self.alpha > 1:
@@ -55,6 +57,78 @@ class ScalarInterval(Interval):
 			plt.scatter(x,-.001,c='r',marker='x',s=50)
 		plt.yticks([])
 		plt.ylim(-.0015,.0005)
+
+
+class FunctionInterval(Interval):
+
+	def __init__(self,samples,alpha,start=1e-6,tol=1e-6,maxiter=100):
+		Interval.__init__(self,samples,alpha,ndim=2)
+
+		p = samples.shape[0]
+		alphaPotential = 1.*np.arange(self.n)/self.n
+		self.alpha = filter(lambda x: abs(x-self.alpha)==abs(self.alpha-alphaPotential).min(),alphaPotential)[0]
+
+		self.mean = samples.mean(0)
+		self.std = samples.std(0)
+		self.lb,self.ub = self.mean-2*self.std,self.mean+2*self.std
+
+		self.epsilon = start
+
+		# function to calculate the empirical interval alpha for a given epsilon, x
+		check = lambda x: 1.*sum((self.samples>self.lb-x).all(1) & (self.samples<self.ub+x).all(1))/self.n
+
+		bounds = []
+
+		# double to find lower and upper epislon bound
+		while check(self.epsilon)<self.alpha:
+			bounds.append((self.epsilon,check(self.epsilon)))
+			self.epsilon *= 2
+
+		bounds.append((self.epsilon,check(self.epsilon)))
+
+		# binary search
+		eLb,eUb = self.epsilon/2,self.epsilon
+		i = 0
+		while True:
+			if abs(check(eLb)-alpha)<tol:
+				self.epsilon = eLb
+				break
+			elif abs(check(eUb)-alpha)<tol:
+				self.epsilon = eUb
+				break
+
+			nb = (eLb + eUb)/2
+
+			if check(nb)<alpha:
+				eLb = nb
+				bounds.append((nb,check(nb)))
+			else:
+				eUb = nb
+				bounds.append((nb,check(nb)))
+
+			i+=1
+			if i > maxiter:
+				self.epsilon = eLb
+				break
+
+		self.bounds = bounds
+
+		self.lb = self.mean - self.std - self.epsilon
+		self.ub = self.mean + self.std + self.epsilon
+
+		#return epsilon,bounds
+
+	def plot(self,x=None,alpha=.2,c='b'):
+		import matplotlib.pyplot as plt
+
+		if x is None:
+			x = np.arange(self.mean.shape[0])
+
+		plt.plot(x,self.mean,color=c)
+		plt.fill_between(x,self.lb,self.ub,alpha=alpha,color=c)
+
+	def contains(self,x):
+		return (x>self.lb).all() & (x<self.ub).all()
 
 
 def functionInterval(samples,start=1e-6,alpha=.95,tol=1e-6,maxiter=100):
