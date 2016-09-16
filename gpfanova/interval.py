63bc0bbdb22b50f6184bd6d96f139c171efcc686
Peter <peter.tonner@duke.edu>
2016-09-16 12:40:25 -0400

    change interval building to better match possible alphas

--- gpfanova/interval.py
+++ gpfanova/interval.py
@@ -1,5 +1,11 @@
+import numpy as np
 
-def functionInterval(samples,start=1e-6,alpha=.95,tol=1e-6):
+def functionInterval(samples,start=1e-6,alpha=.95,tol=1e-6,maxiter=100):
+
+	# change alpha to be best possible given the number of samples
+	p = samples.shape[0]
+	alphaPotential = 1.*np.arange(p)/p
+	alpha = filter(lambda x: abs(x-alpha)==abs(alpha-alphaPotential).min(),alphaPotential)[0]
 
 	mean = samples.mean(0)
 	std = samples.std(0)
@@ -10,12 +16,18 @@ def functionInterval(samples,start=1e-6,alpha=.95,tol=1e-6):
 	# function to calculate the empirical interval alpha for a given epsilon, x
 	check = lambda x: 1.*sum((samples>lb-x).all(1) & (samples<ub+x).all(1))/samples.shape[0]
 
+	bounds = []
+
 	# double to find lower and upper epislon bound
 	while check(epsilon)<alpha:
+		bounds.append((epsilon,check(epsilon)))
 		epsilon *= 2
+
+	bounds.append((epsilon,check(epsilon)))
 
 	# binary search
 	eLb,eUb = epsilon/2,epsilon
+	i = 0
 	while True:
 		if abs(check(eLb)-alpha)<tol:
 			epsilon = eLb
@@ -28,7 +40,14 @@ def functionInterval(samples,start=1e-6,alpha=.95,tol=1e-6):
 
 		if check(nb)<alpha:
 			eLb = nb
+			bounds.append((nb,check(nb)))
 		else:
 			eUb = nb
+			bounds.append((nb,check(nb)))
 
-	return epsilon
+		i+=1
+		if i > maxiter:
+			epsilon = eLb
+			break
+
+	return epsilon,bounds
